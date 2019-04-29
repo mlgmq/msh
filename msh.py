a64174beca519e1911ba5237659ec68ca9419f8a
mlgmq <mlgmq@163.com>
2019-04-29 15:52:53 +0800

    run command text file
    run command text file

--- msh.py
+++ msh.py
@@ -27,21 +27,7 @@ def print_output(host_info):
             fileHandler.write(line)
     fileHandler.close()
 
-def uploadfile(sshclient, local_path, remote_path):
-    try:
-        sftp = paramiko.SFTPClient.from_transport(sshclient.get_transport())
-        sftp.put(local_path, remote_path)
-    except Exception as e:
-        print(e.message)
-
-def downloadfile(sshclient, local_path, remote_path):
-    try:
-        sftp = paramiko.SFTPClient.from_transport(sshclient.get_transport())
-        sftp.get(remote_path, local_path)
-    except Exception as e:
-        print(e.message)
-
-    
+  
 class SSHConnect():
 
     def __init__(self, hostname, username, password, key_filename, connect_timeout):
@@ -118,14 +104,14 @@ class mshRun(object):
     def __init__(self, processes=2):
         self.version = '0.1'
         self.processes = processes
-        self.connect_timeout = 3
+        self.connect_timeout = 40
         self.host_details = []
         self.workdir = os.path.abspath(os.curdir)
-        self.default_hostfile = self.__trailing('host_list', 'hosts')        
+        self.default_hostfile = self.__trailing('host_list', 'hosts')  
+        self.default_cmd_file = self.__trailing('run_cmds', '')        
         self.output_file_path = self.__trailing(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), 'output')
         self.deps_files = []
-        _cmd = [ 'id', 'pwd', 'cd /tmp', 'pwd']
-        self.__cmd = ";".join(_cmd)
+        self.__cmd = "true"
 
     def add_ssh_host(self, host, user=None, password=None, key_filename=None):
 #        print('host:' + host + ' user:' + user + ' password:' + password + ' key_filename:' + key_filename)
@@ -148,7 +134,7 @@ class mshRun(object):
         
     def __load_hosts(self, host_filename):
         filename = self.__trailing(host_filename, 'hosts')
-        print("__load_hosts:", filename)
+        print("load_hosts:", filename)
         try:
             fileParser = open(filename, 'r')
         except IOError:
@@ -167,6 +153,30 @@ class mshRun(object):
                     keyfile = ''
                 if len(host) >= 3 :
                     self.add_ssh_host(host[0], host[1] , host[2], keyfile)
+
+    def __load_cmd_files(self, cmd_filename):
+        filename = self.__trailing(cmd_filename)
+        print("load_cmd_files:", filename)
+        try:
+            fileParser = open(filename, 'r')
+        except IOError:
+            print("[!] Could not open file " + filename)
+            return None 
+        except:
+            print("[!] Could not access file " + filename)
+            return None
+        try:
+            while True:
+                line = fileParser.readline()
+                line = line.replace('\n', '')
+                if line and (line[0:1] != '#'):
+                    self.__cmd = self.__cmd + ";" + line
+                else:
+                    break
+        finally:
+            fileParser.close()
+
+        print("cmd::::" + self.__cmd)        
 
     def __load_deps_files(self, deps_filename):
         filename = self.__trailing(deps_filename)
@@ -187,13 +197,13 @@ class mshRun(object):
                     self.deps_files.append({'ops':r[0], 'localpath': r[1], 'remotepath': r[2]})
 
     def __start(self):
-        opts, args = getopt.getopt(sys.argv[1:], '-h-f:-v-p:-t:-o:-d:', ['help', 'filename=', 'version', 'process=', 'connect_timeout=', 'output=', 'deps_files='])
+        opts, args = getopt.getopt(sys.argv[1:], '-h-f:-v-p:-t:-o:-d:-c:', ['help', 'filename=', 'version', 'process=', 'connect_timeout=', 'output=', 'deps_files=', 'cmd_file='])
         for opt_name, opt_value in opts:
             if opt_name in ('-h', '--help'):
                 print(sys.argv[0] + ' [-h] [-v]  [-p process] [-t connect_timeout]')
-                print('    [-f host_filename] [-o output_file_path] [-d deps_files]')
+                print('    [-f host_filename] [-o output_file_path] [-d deps_files] [-c cmd_file]')
                 print(sys.argv[0] + '[--help] [--version] [--process process] [--connect_timeout connect_timeout]')
-                print('    [--filename host_filename] [--output output_file_path]  [--deps_files deps_files]')
+                print('    [--filename host_filename] [--output output_file_path]  [--deps_files deps_files] [--cmd_file cmd_file]')
                 sys.exit()
             if opt_name in ('-v', '--version'):
                 print("[*] Version is {} ".format(self.version))
@@ -209,12 +219,18 @@ class mshRun(object):
                 self.output_file_path = self.__trailing(opt_value, 'output')
             if opt_name in ('-d', '--deps_files'):
                 self.__load_deps_files(opt_value)
+            if opt_name in ('-c', '--cmd_file'):
+                self.default_cmd_file = ''
+                self.__load_cmd_files(opt_value)
 
     def run(self):
         self.__start()
+        if self.default_cmd_file != '' :
+            self.__load_cmd_files(self.default_cmd_file)
         self.__load_deps_files('deps_files')
         if self.default_hostfile != '' :
             self.__load_hosts(self.default_hostfile)
+                    
         pool = multiprocessing.Pool(processes=self.processes)
         try:
             for host_info in self.host_details:
@@ -226,38 +242,59 @@ class mshRun(object):
             pool.terminate()
             raise 'run error!'
 
-def Parsing_Path(host,path):
-    path=path
-    path=path.replace('$CURRDIR',os.path.abspath(os.curdir))
-    path=path.replace('$HOST', host['host'])
-    path=path.replace('$OUTPUT_PATH',host['output_file_path'])
+
+def Parsing_Path(host, p_path):
+    path = p_path
+    path = path.replace('$CURRDIR', os.path.abspath(os.curdir))
+    path = path.replace('$HOST', host['host'])
+    path = path.replace('$OUTPUT_PATH', host['output_file_path'])
     return path
 
 
+def transfer_file(host_info, to_srv='up'):
+    try:
+        try:
+            t = paramiko.Transport((host_info['host'], 22))
+            if host_info['key_filename'] == '' :
+                t.connect(username=host_info['user'], password=host_info['password'])
+            else:
+                private_key = paramiko.RSAKey.from_private_key_file(host_info['key_filename'])
+                t.connect(username=host_info['user'], pkey=private_key)
+            sftp = paramiko.SFTPClient.from_transport(t)
+            for script in host_info['deps_files']:
+                if to_srv == 'up' and script['ops'] == 'up':
+                    sftp.put(Parsing_Path(host_info, script['localpath']), Parsing_Path(host_info, script['remotepath']))
+                if to_srv == 'down' and script['ops'] == 'down':
+                    sftp.get(Parsing_Path(host_info, script['remotepath']), Parsing_Path(host_info, script['localpath']))
+            t.close()
+        except Exception as e:
+            print(e.message)
+    except:
+        pass
+
+
 def _run_ssh_cmd(host_info):
-    sshcon = SSHConnect(
-              host_info['host'],
-              host_info['user'],
-              host_info['password'],
-              host_info['key_filename'],
-              host_info['connect_timeout']
-              )
-    sshcon.connect()
-    if host_info['deps_files'] :
-        for script in host_info['deps_files']:
-            if script['ops'] == 'up':
-                uploadfile(sshcon.__sshclient, Parsing_Path(host_info, script['localpath']), script['remotepath'])
-    output = sshcon.cmd(host_info['cmd'])
-    if host_info['deps_files'] :
-        for script in host_info['deps_files']:
-            if script['ops'] == 'down':
-                downloadfile(sshcon.__sshclient, Parsing_Path(host_info, script['localpath']), script['remotepath'])
-    sshcon.close()
-    # host_info['status'] = sshcon.status
-    # host_info['output'] = output
-    host_info.update({ 'status': sshcon.status  })
-    host_info.update({ 'output': output  })
-    return host_info
+    try:
+        sshcon = SSHConnect(
+                  host_info['host'],
+                  host_info['user'],
+                  host_info['password'],
+                  host_info['key_filename'],
+                  host_info['connect_timeout']
+                  )
+        sshcon.connect()
+        if sshcon.connected():
+            transfer_file(host_info, 'up')
+        output = sshcon.cmd(host_info['cmd'])
+        if sshcon.connected():
+            transfer_file(host_info, 'down')
+        # host_info['status'] = sshcon.status
+        # host_info['output'] = output
+        host_info.update({ 'status': sshcon.status  })
+        host_info.update({ 'output': output  })
+        sshcon.close()
+    finally:
+        return host_info
 
 
 if __name__ == '__main__':
